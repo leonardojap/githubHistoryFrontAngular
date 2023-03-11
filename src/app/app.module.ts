14f3d0eb5936c81b78afb1d49fe6c373476d12ad
Leonardo Alvarado <ing.leonardo92@gmail.com>
2023-03-11 12:38:57 -0400

    fix import module method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListMethodsComponent } from './backMethods/list-methods/list-methods.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [AppComponent, ListMethodsComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
